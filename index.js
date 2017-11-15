09f09fa5553943c50a9442cf456ebfc513d9b5b9
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-11-15 04:46:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+function scuberGreetingForFeet(something){
+  if(something >==2500){
+    return 'No can do.';
+  }else
+  if(something <== 400){
+    return 'This one is on me!';
+  }else if (something>=2000) {
+    return 'I will gladly take your thirty bucks.';
+}
+}
+
+function ternaryCheckCity(something){
+
+}
